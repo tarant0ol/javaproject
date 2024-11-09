85a87d25a8395028d9c4eabd8f12975596bdd093
taran <tarantool777@gmail.com>
2024-11-09 17:20:00 +0300

    pr29

--- pr29/Main.java
+++ pr29/Main.java
@@ -2,18 +2,14 @@ package pr29;
 
 import pr20.T5.Matrix;
 
-import java.net.Inet4Address;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //System.out.println("Enter Graph(matrix) size: ");
         int n = Integer.parseInt(sc.nextLine());
         Matrix<Integer> matrix = new Matrix<>(n, n);
 
-
-        //System.out.println("Enter matrix:");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 int num = sc.nextInt();
